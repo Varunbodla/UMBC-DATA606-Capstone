561191ef2dfc5ecd3dfbda6ed6d268c44b8532a9
Varunbodla <85016388+Varunbodla@users.noreply.github.com>
2024-05-20 06:18:06 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -66,7 +66,7 @@ def translate(input_text):
     with open('app/tokenizer_eng.pickle', 'rb') as file:
         tokenize_eng = pickle.load(file)
     input_sequence    = pad_sequences(tokenize_eng.texts_to_sequences([input_sequence]), maxlen=max_len_eng, dtype='int32', padding='post')
-    model.load_weights('app/custom_model.h5')
+    #model.load_weights('app/custom_model.h5')
     en_h,en_c         = model.layers[0].initialize_states(1)
     en_outputs        = model.layers[0](tf.constant(input_sequence), [en_h,en_c])
     de_input          = tf.constant([[word2idx_outputs['<start>']]])
